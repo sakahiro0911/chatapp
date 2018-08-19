8875a70d7d6fbcd5a8df17a4cd8aa265835a52f2
sakahiro0911 <YHE00327@nifty.com>
2018-08-19 10:52:39 +0900

    1

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -55,11 +55,12 @@ public func routes(_ router: Router, _ wss: NIOWebSocketServer ) throws {
 //            print("onText=\(text)")
             let data: Data? = text.data(using: .utf8)
             
-             ws.send("data=\(data!)")
+          
 //                       let json = try JSONSerialization.jsonObject(with: text!, options: JSONSerialization.ReadingOptions.mutableContainers) as!
             do {
             let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-            
+               ws.send("json=\(json)")
+                
             if let u = (json["username"] as? String) {
                 print("username(save)=\(u)")
                 username = u
